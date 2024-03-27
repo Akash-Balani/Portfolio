a79b3c236083c3537f1e123bb47f29a8c6e87602
Akash-Balani <ajbalani593@gmail.com>
2024-03-27 10:03:26 +0530

    adding code

--- P_script.js
+++ P_script.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-  
-    menuToggle.addEventListener('click', function() {
-      navMenu.classList.toggle('show');
-    });
-  });
-  
+
